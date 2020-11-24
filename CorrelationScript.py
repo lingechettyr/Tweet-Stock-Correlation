4e61d402487acbde9c35a5a3b9cefabac1771d17
Rakesh <lingechettyr@ufl.edu>
2020-11-23 22:13:59 -0500

    Export Notebook to .py file

---
+++ CorrelationScript.py
@@ -0,0 +1,29 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+#Import relevant APIs
+import numpy as np
+import pandas as pd
+import tweepy as tw
+import matplotlib.pyplot as plt
+
+
+# %%
+#Twitter Authentication
+from tweepy import OAuthHandler
+from tweepy import API
+
+apiKey = 'sUY1UAhfmauoDAqodxPJCmE1d'
+apiSecret = 'nRrYuBn8X1MauIFfyznAXdwdLsTcVReytlIUpsfjiI2GncodX0'
+accessToken = '3295318197-LMzVBdXwZPbIgJU8Fd0ksjT8mEOES2HRYC2J2ha'
+accessSecret = 'RgqkfaXowXGk1JaKs0jTOIvGVjZbGnL32bsPk4DuzohBf'
+
+auth = OAuthHandler(apiKey, apiSecret)
+auth.set_access_token(accessToken, accessSecret)
+api = API(auth)
+
+
+# %%
+#Searching and Storing
+
+
