d70fd753ac792609f2967dc1b2288d596ed37f4c
Rakesh <lingechettyr@ufl.edu>
2020-12-06 03:25:57 -0500

    Update to match .ipynb file

--- src/CorrelationScript_JL.py
+++ src/CorrelationScript_JL.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 #Import relevant APIs
 import numpy as np
 import pandas as pd
@@ -9,10 +10,11 @@ import sys
 import matplotlib.pyplot as plt
 from datetime import date
 from datetime import timedelta
-from sklearn.linear_model import LinearRegression
+from time import process_time
 
 
 # Setting up Twitter Authentication:
+
 from tweepy import OAuthHandler
 from tweepy import API
 
@@ -26,8 +28,16 @@ auth.set_access_token(accessToken, accessSecret)
 api = API(auth, wait_on_rate_limit = True)
 
 
-# Getting Ticker data and storing in Pandas DataFrame, then dict with only percent change:
-tickers = ['TSLA','AAPL', 'FB', 'ZM']
+# Storing methods: Getting Ticker data and storing in Pandas DataFrame, then dict with only percent change:
+
+tickers = ['TSLA','AAPL', 'FB', 'ZM', 'ADBE', 'ADI', 'ADP', 'ADSK', 'ALGN', 'ALXN', 'AMAT', 'AMD', 'AMGN', 'AMZN',
+          'ANSS', 'ASML', 'ATVI', 'AVGO', 'BIDU', 'BIIB', 'BKNG', 'BMRN', 'CDNS', 'CDW', 'CERN', 'CHKP', 'CHTR', 'CMCSA',
+          'COST', 'CPRT', 'CSCO', 'CSX', 'CTAS', 'CTSH', 'CTXS', 'DLTR', 'DOCU', 'DXCM', 'EA', 'EBAY', 'EXC', 'EXPE',
+          'FAST', 'FISV', 'FOX', 'GILD', 'GOOGL', 'IDXX', 'ILMN', 'INCY', 'INTC', 'INTU', 'ISRG', 'JD', 'KDP', 'KHC',
+          'KLAC', 'LBTYA', 'LRCX', 'LULU', 'MAR', 'MCHP', 'MDLZ', 'MELI', 'MNST', 'MRNA', 'MSFT', 'MU', 'MXIM',
+          'NFLX', 'NTES', 'NVDA', 'NXPI', 'ORLY', 'PAYX', 'PCAR', 'PDD', 'PEP', 'PYPL', 'QCOM', 'REGN', 'ROST', 'SBUX',
+          'SGEN', 'SIRI', 'SNPS', 'SPLK', 'SWKS', 'TCOM', 'TMUS', 'TTWO', 'TXN', 'ULTA', 'VRSK', 'VRSN', 'VRTX',
+          'WBA', 'WDAY', 'XEL', 'XLNX']
 perChange = {}
 
 def storePercentChange():
@@ -35,7 +45,7 @@ def storePercentChange():
     global days_prior
     global start_date
     end_date = date.today()
-    days_prior = input("Enter the number of days to retrieve market data (max 7 days, min 2 days):  ")
+    days_prior = input("Enter the number of days to retrieve selected ticker & tweet data (max 7 days, min 2 days):  ")
     start_date = end_date - timedelta(days=int(days_prior))
     weekno = start_date.weekday()
     if weekno < 5 and int(days_prior) <= 7 and int(days_prior) >= 2:
@@ -50,7 +60,65 @@ def storePercentChange():
         sys.exit("Input does not match constraints. Exiting...")
 
 
+# Sorting Algorithms for all tickers in NQ100:
+
+ticker_PerList = []
+
+def makePairs():
+    for index, (key,value) in enumerate(perChange.items()):
+        ticker_PerList.insert(index,(value,key))
+        
+def selectionSort(ticker):
+    length = len(ticker)
+    for index in range(length): 
+        largest = index 
+        for select in range(index+1, length): 
+            if ticker[largest][0] < ticker[select][0]: 
+                largest = select         
+        ticker[index], ticker[largest] = ticker[largest], ticker[index] 
+        
+def heapify(ticker, n, index): 
+    smallest = index  
+    left = 2 * index + 1      
+    right = 2 * index + 2     
+    if left < n and ticker[left][0] < ticker[smallest][0]: 
+        smallest = left
+    if right < n and ticker[right][0] < ticker[smallest][0]: 
+        smallest = right
+    if smallest != index: 
+        ticker[index],ticker[smallest] = ticker[smallest],ticker[index]  # swap 
+        heapify(ticker, n, smallest) 
+
+def heapSort(ticker): 
+    length = len(ticker) 
+    for index in range(length // 2 - 1, -1, -1): 
+        heapify(ticker, length, index) 
+    for index in range( length-1, 0, -1): 
+        ticker[index], ticker[0] = ticker[0], ticker[index]    
+        heapify(ticker, index, 0) 
+
+
+# Selecting Top 5 & Bottom 5 Tickers in terms of % change and storing in list and dict:
+
+sortedPerChange = {}
+perChangeSelected = {}
+
+def storeSelectTickers():
+    global selected_tickers
+    selected_tickers = ticker_PerList[:5]
+    selected_tickers += ticker_PerList[-5:]
+    
+def storePerSorted():
+    for x in range(len(ticker_PerList)):
+        sortedPerChange[ticker_PerList[x][1]] = ticker_PerList[x][0]
+    
+def storeSelectPer():
+    for x in range(len(selected_tickers)):
+        perChangeSelected[selected_tickers[x][1]] = selected_tickers[x][0]
+
+
 # Searching Twitter tweets using Tweepy:
+
 tweetsDict = {}
 
 def searchTweets():
@@ -58,7 +126,7 @@ def searchTweets():
     count = 0
     counter = 0
     maxID = 0
-    for tick in tickers:
+    for tick in perChangeSelected:
         while counter < 10:
             if maxID > 0:
                 public_tweets = api.search(q = '$'+tick+' -filter:retweets', count = 100, 
@@ -69,28 +137,55 @@ def searchTweets():
             count += len(public_tweets)
             maxID = public_tweets[-1].id
             counter += 1
-        tweetsDict[tick] = count - 9 #Account for replicate tweets from using maxID
+        tweetsDict[tick] = count 
         counter = 0
         count = 0
         maxID = 0
 
 
 # Merging two dicts on Ticker axis, storing in DF and Plotting:
+
+def printPerChange():
+    initial_all_tickers = pd.DataFrame(sortedPerChange.items())
+    initial_all_tickers.columns = ['Ticker', '% Change']
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    print(initial_all_tickers.to_string(index=False))
+    
 def mergeAndPlot():
-    corr_list = [perChange, tweetsDict]
+    corr_list = [perChangeSelected, tweetsDict]
     correlation = {}
 
-    for tick in perChange.keys():
+    for tick in perChangeSelected.keys():
         correlation[tick] = tuple(correlation[tick] for correlation in corr_list)
-
+    
     initial_table = pd.DataFrame(correlation)
     final_table = initial_table.transpose()
     final_table.columns = ['% Change', 'Number of Tweets']
     print(final_table)
-    final_table.plot(kind = 'scatter', x='Number of Tweets', y='% Change', title = 'Tweets vs. % Change', color = 'red')
+    final_table.plot(kind = 'scatter', x='Number of Tweets', y='% Change', title = 'Tweets vs. % Change')
+    
+    
+# Function Calls
 
-
+start_time = process_time()
 storePercentChange()
+
+print("1. Selection Sort")
+print("2. Heap Sort")
+select_sort = input("Select sorting algorithm to sort tickers by % change in descending order (1 or 2): ")
+
+if int(select_sort) == 1:
+    makePairs()
+    selectionSort(ticker_PerList)
+elif int(select_sort) == 2:
+    makePairs()
+    heapSort(ticker_PerList)
+    
+storePerSorted()
+printPerChange()
+storeSelectTickers()
+storeSelectPer()
 searchTweets()
 mergeAndPlot()
-
+end_time = process_time()
+print("Time taken to run program in seconds, depending on sorting algorithm selected: ", end_time - start_time)
